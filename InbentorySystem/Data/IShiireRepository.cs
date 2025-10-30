a6f7efefbdcb3237e17ac82d5ec5096eec2c1167
enomooo <erumoo3@gmail.com>
2025-10-30 17:56:41 +0900

    2025/10/30追加分

---
+++ InbentorySystem/Data/IShiireRepository.cs
@@ -0,0 +1,22 @@
+﻿using InbentorySystem.Data.Models;
+
+namespace InbentorySystem.Data
+{
+    public interface IShiireRepository
+    {
+        // 仕入伝票の登録と在庫の更新を伴う
+        Task<int> RegisterAsync(ShiireModel shiire);
+
+        // 仕入伝票の検索
+        Task<List<ShiireModel>> SearchAsync(string dateFrom, string dateTo, string shohinCode);
+
+        // 特定の仕入伝票を取得
+        Task<ShiireModel> GetByDateAndCodeAsync(string date, string code);
+
+        // 修正（数量や仕入先コードの変更、在庫の調整を伴う）
+        Task<int> UpdateAsync(ShiireModel shiire);
+
+        // 削除　(仕入伝票の削除と在庫の調整を伴う)
+        Task<int> DeleteAsync(string date, string code, string quantity);
+    }
+}
---
+++ InbentorySystem/Data/Models/ShiireModel.cs
@@ -0,0 +1,14 @@
+﻿namespace InbentorySystem.Data.Models
+{
+    // T_SHIIREテーブルに対応
+    public class ShiireModel
+    {
+        public string ShiireNengappi { get; set; }
+        public string ShohinCode { get; set; }
+
+        public string ShiiresakiCode { get; set; }
+        public int Quantity { get; set; }
+        public decimal Shiirene { get; set; }
+        public DateTime Tourokunichiji { get; set; }
+    }
+}
